d4538c913af6ab8c6c87c46aa138940db3565bd7
Skrpion <nestor.martinez.c@uniautonoma.edu.co>
2024-02-24 17:37:57 -0500

    🎉 [ADD]

---
+++ _next/static/XSe6_RzH2FbEfcRaaL7by/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fauth\u002Fchange-password","\u002Fauth\u002Freset-password","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fcareers\u002F[slug]","\u002Fchangelog","\u002Fchangelog\u002F[slug]","\u002Fcustomers\u002F[slug]","\u002Flegal\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
